4517b43f8048d13fb6b96c0e0ebfbcadb3eb50fd
Susmitha <sushmithaeluri@gmail.com>
2024-06-17 11:42:47 -0400

    Database migration with Liquibase

---
+++ SpringWithLiquibase/src/main/java/com/example/SpringWithLiquibase/SpringWithLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringWithLiquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithLiquibaseApplication.class, args);
+	}
+
+}
---
+++ SpringWithLiquibase/src/test/java/com/example/SpringWithLiquibase/SpringWithLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringWithLiquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
